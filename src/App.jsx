a9029b1e08cceeaf58be575f5e16dd0d38c0d018
unknown <PeterDiyo>
2024-04-08 23:30:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Welcome to submenus. <br />
+          Navigate
+        </h1>
+        <p>
+          submenus is an app that uses context api to control navnar and
+          sidebar, it is 100% responsive
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
